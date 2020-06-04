d19c67ba5cb1d68d45c6ba49c41819ec32cb5631
ritika-0111 <51761546+ritika-0111@users.noreply.github.com>
2020-06-05 01:14:23 +0530

    add credentials

--- module/sms.js
+++ module/sms.js
@@ -1,5 +1,5 @@
-const accountSid = 'AC6cd9784fdd8dddc570d76e0967bbebba';
-const authToken = 'dc3fdd66a227b8e4f4ec8836287bd488';
+const accountSid = '*********************************';
+const authToken = '**************************************';
 const client = require('twilio')(accountSid, authToken);
 
 
@@ -7,7 +7,7 @@ function sendSMS(body,to) {
   client.messages
     .create({
       body: body,
-      from: '+12052559217',
+      from: '+1**********',
       to: to
     })
     .then(message => console.log(message.sid));
